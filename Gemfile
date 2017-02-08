ebe7dacb7dbac87c5be4d9539fdba3394e5b207f
Angelos Orfanakos <me@agorf.gr>
2017-02-08 11:15:33 +0200

    Add script to generate OAuth access token

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
 gem 'dotenv'
+gem 'launchy'
 gem 'nokogiri'
 gem 'oauth'
---
+++ generate_oauth_access_token.rb
@@ -0,0 +1,24 @@
+require 'dotenv'
+require 'launchy'
+require 'oauth'
+
+Dotenv.load
+
+request_token = OAuth::Consumer.new(
+  ENV.fetch('GOODREADS_API_KEY'),
+  ENV.fetch('GOODREADS_API_SECRET'),
+  site: 'http://www.goodreads.com'
+).get_request_token
+
+puts "Opening #{request_token.authorize_url}"
+Launchy.open(request_token.authorize_url)
+
+puts
+puts 'Press ENTER after you have authorized the app'
+gets
+
+access_token = request_token.get_access_token
+puts 'Place the following into your .env file:'
+puts
+puts "OAUTH_ACCESS_TOKEN=#{access_token.token}"
+puts "OAUTH_ACCESS_SECRET=#{access_token.secret}"
